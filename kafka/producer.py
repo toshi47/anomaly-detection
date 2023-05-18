5cff68ec668c4eeac7110a6bba5b3e4024bc714d
toshi47 <toshiro280701@gmail.com>
2023-05-18 06:03:00 -0700

    traffic sample producer added

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,33 +1,28 @@
-from numpy.random import choice, randint
 from kafka import KafkaProducer    
 import json
+import pandas
 
 def get_random_value():
-    new_dict = {}
-
-    branch_list = ['Kazan', 'SPB', 'Novosibirsk', 'Surgut']
-    currency_list = ['RUB', 'USD', 'EUR', 'GBP']
-
-    new_dict['branch'] = choice(branch_list)
-    new_dict['currency'] = choice(currency_list)
-    new_dict['amount'] = randint(-100, 100)
-
-    return new_dict
+    df=pandas.read_csv('kafka/dataset_sdn.csv')
+    row=df.sample()
+    dict=row.to_dict('records')[0]
+    return dict
  
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                             value_serializer=lambda x:json.dumps(x).encode('utf-8'),
-                             compression_type='gzip')
-my_topic = 'transaction'
-data = get_random_value()
+if __name__ == "__main__":
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                                value_serializer=lambda x:json.dumps(x).encode('utf-8'),
+                                compression_type='gzip')
+    my_topic = 'transaction'
+    data = get_random_value()
 
-try:
-    future = producer.send(topic = my_topic, value = data)
-    record_metadata = future.get(timeout=10)
-    print('--> The message has been sent to a topic: {}, partition: {}, offset: {}' .format(record_metadata.topic,record_metadata.partition, record_metadata.offset ))   
-                             
-except Exception as e:
-    print('--> It seems an Error occurred: {}'.format(e))
+    try:
+        future = producer.send(topic = my_topic, value = data)
+        record_metadata = future.get(timeout=10)
+        print('--> The message has been sent to a topic: {}, partition: {}, offset: {}' .format(record_metadata.topic,record_metadata.partition, record_metadata.offset ))   
+                                
+    except Exception as e:
+        print('--> It seems an Error occurred: {}'.format(e))
 
-finally:
-    producer.flush()
+    finally:
+        producer.flush()
