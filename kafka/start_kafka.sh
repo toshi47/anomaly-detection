a32bd756d13c7a935127a911b22193cea59047e4
toshi47 <toshiro280701@gmail.com>
2023-06-05 07:48:02 -0700

    script edited

--- kafka/start_kafka.sh
+++ kafka/start_kafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source .env
-$KAFKA_PATH/bin/zookeeper-server-start.sh -daemon /home/user/kafka/config/zookeeper.properties
-$KAFKA_PATH/bin/kafka-server-start.sh -daemon /home/user/kafka/config/server.properties
+$KAFKA_PATH/bin/zookeeper-server-start.sh -daemon $KAFKA_PATH/config/zookeeper.properties
+$KAFKA_PATH/bin/kafka-server-start.sh -daemon $KAFKA_PATH/config/server.properties
 $KAFKA_PATH/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic transaction
